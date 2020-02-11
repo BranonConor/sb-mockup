f20c365c8f98a66f98796a1b43aac1e59994ce12
branonconor <breusebio@westmont.edu>
2020-02-11 13:22:04 -0800

    MInor changes to card design

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-// GLOBALS
+// --------------------------- GLOBALS --------------------------------
 const hamburger = document.querySelector('.hamburger');
 const navMobile = document.querySelector('.nav-mobile');
 const mobileLinks = document.querySelectorAll('.mobileLink');
-
-// HELPER FUNCS
+const topPhone = document.querySelector('.strip-info');
+const page = document.querySelector('html');
+// ------------------------- HELPER FUNCS --------------------------------
 const expandMenu = () => {
     // take the mobile nav menu and reveal it on a hamburger click
     navMobile.classList.toggle('revealNavMobile');
@@ -14,15 +15,24 @@ const showMobileMenu = () => {
         mobileLinks[i].classList.toggle('display-none');
     }
 }
+//const moveAndFix = () => {
+//    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//      topPhone.classList.toggle('move-and-fix');
+//    }
+//}
 
 // Burger animation
 const hamburgerClick = (x) => {
     x.classList.toggle('change');
 }
 
-
 // Expand Mobile Menu
 hamburger.addEventListener('click', function() {
     expandMenu();
     setTimeout(showMobileMenu, 250);
 });
+
+// Scroll Events for Top Strip 
+//window.onscroll = function() {
+//    moveAndFix();
+//}
